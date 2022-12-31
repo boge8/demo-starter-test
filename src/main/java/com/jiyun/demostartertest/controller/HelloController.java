8fe8b3369083ff609ffbd2fb10c53df033621020
boge <boge@usian.com>
2022-12-31 22:33:18 +0800

    第一次提交

---
+++ src/main/java/com/jiyun/demostartertest/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.jiyun.demostartertest.controller;
+
+import com.jiyun.hello.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class HelloController {
+    @Autowired
+    HelloService helloService;
+    @GetMapping("/h1")
+    public String h1(HttpServletResponse response){
+        String say = helloService.say();
+        return say;
+    }
+}
